69925b4813121ced7ead2dd09b21c5e9dd8aa18c
Jason Frazier <jasonxfrazier@gmail.com>
2025-06-09 16:18:11 -0400

    getting ci pipeline working

--- example.py
+++ example.py
@@ -1,22 +1,21 @@
-from puddy.trajectory_collection import TrajectoryCollection, ColumnConfig
-from puddy.trajectory_analyzer import TrajectoryAnalyzer, visualize_trajectories_sample
 import matplotlib.pyplot as plt
 import numpy as np
+
+from puddy.trajectory_analyzer import TrajectoryAnalyzer, visualize_trajectories_sample
+from puddy.trajectory_collection import ColumnConfig, TrajectoryCollection
+
 
 def main() -> None:
     # Adjust column names to match your CSV headers
     geo_config: ColumnConfig = ColumnConfig.create_geo(
-        lon_col='lon',
-        lat_col='lat',
-        alt_col='alt',
-        identifier_col='identifier'
+        lon_col="lon", lat_col="lat", alt_col="alt", identifier_col="identifier"
     )
     collection = TrajectoryCollection()
-    collection.load_from_file('somegeodata.csv', config=geo_config)
+    collection.load_from_file("somegeodata.csv", config=geo_config)
     collection.visualize_sample(5)
 
     analyzer = TrajectoryAnalyzer(collection)
-    analyzer.train_anomaly_detector(method='isolation_forest')
+    analyzer.train_anomaly_detector(method="isolation_forest")
     scores: np.ndarray = analyzer.get_normalcy_scores()
     # You can access anomalies here if you want to print or process them, otherwise omit
     # anomalies = analyzer.find_anomalies(threshold=0.2)
@@ -28,9 +27,10 @@ def main() -> None:
         analyzer.collection.trajectories,
         scores,
         normal_sample=20,
-        show_all_anomalies=True
+        show_all_anomalies=True,
     )
     plt.show()
+
 
 if __name__ == "__main__":
     main()
